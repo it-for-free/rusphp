e8c9bf9a8205736b9c9bbea58e51373a59ecc7bd
vedro-compota <ru-ibn-hottab@yandex.ru>
2017-12-19 16:11:09 +0300

    Добавлен  класс для тестирования автозагрузки

---
+++ Test.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ItForFree\rusphp;
+
+
+/**
+ * тестируем работу автозагрузки)
+ */
+class Test
+{
+    public static function test()
+    {
+        echo 'hello) its rusphp'; die();
+    }
+    
+}
