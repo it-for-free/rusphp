0e00713930c2fdba0f405a9c9f307dec7f1fc007
vedro-compota <ru-ibn-hottab@yandex.ru>
2018-01-29 18:40:12 +0300

    Класс для измерения временных дистанций

---
+++ PHP/DateTime/DateTimeDiffirence.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace rusphp\PHP\DateTime;
+
+use rusphp\PHP\Str\StrCommon as Str;
+use rusphp\Log\SimpleEchoLog as log;
+
+/**
+ * Для измерения дистанции между двумя датами
+ * -- обёртка над \DateTime
+ * 
+ * Полезно посмотреть:
+ * @see https://secure.php.net/manual/ru/datetime.diff.php#97880
+ */
+class DateTimeDifference
+{
+ 
+    /**
+     * Вычислит число лет прошедших к сегодняшнему моенту начиная с даты,
+     * хранящейся в $fromDateTime
+     * (можно использовать для вычисления возраста по дате рождения)
+     * 
+     * @param \DateTime $$startDateTime  точка времени (подразумевается, что в прошлом)
+     * @return int число прошедших лет
+     */
+    public static function yearsByCurrentMoment($startDateTime)
+    {
+        $tz  = new DateTimeZone('Europe/Brussels'); // зона не имеет значения в данном случае
+        $years = $startDateTime
+                ->diff(new DateTime('now', $tz)) 
+                ->y;
+     
+        return $years;
+    }
+    
+    
+    /**
+     * Разница в днях между двумя датами
+     * 
+     * @param \DateTime $startDateTime
+     * @param \DateTime $endDateTime
+     * @return int                     число дней
+     */
+    public static function inDays($startDateTime, $endDateTime)
+    {
+        $days  = $startDateTime->diff($endDateTime)->d;
+        return $days;
+    }
+                   
+}
