07ea505fc00f36c7407244a2fdaf3adcaee71e6c
vedro-compota <ru-ibn-hottab@yandex.ru>
2018-05-14 08:35:18 +0300

    Элементарная обёртка к PHPEXcel

---
+++ patch/PHPExcel/SheetWrapper.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\patch\PHPExcel;
+
+/**
+ * Обёртка над PHPExcel_Worksheet 
+ *
+ * @author qwer
+ */
+class SheetWrapper {
+    
+    /**
+     *
+     * @var PHPExcel_Worksheet 
+     */
+    protected $sheet = null;
+    
+    
+    /**
+     * 
+     * @param PHPExcel_Worksheet $sheet экземпляр класса для работы  с документом
+     */
+    public function __construct(&$sheet) {
+        $this->sheet = $sheet;
+  
+    }
+    
+}
