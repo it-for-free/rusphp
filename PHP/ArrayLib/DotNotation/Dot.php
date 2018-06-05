f3f84c3f7074d22e9a3c4754e58bc4d2ee829575
vedro-compota <ru-ibn-hottab@yandex.ru>
2018-06-05 19:20:31 +0300

    Добавляем точечную нотацию

---
+++ PHP/ArrayLib/DotNotation/Dot.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ItForFree\rusphp\PHP\ArrayLib\DotNotation;
+
+
+/**
+ *  Класс, обеспечивающих доступ к элментам многомерного массива через "через точку"
+ *  (по аналогии 1-ой задачей с @link http://fkn.ktu10.com/?q=node/10151)
+ */
+class Dot extends \Adbar\Dot
+{
+    
+}
