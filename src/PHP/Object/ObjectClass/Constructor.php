efffc19db13fcb0743edfe0ff7868a63b6ff0d69
vedro-compota <ru-ibn-hottab@yandex.ru>
2018-06-27 12:43:28 +0300

    форматирование

--- src/PHP/Object/ObjectClass/Constructor.php
+++ src/PHP/Object/ObjectClass/Constructor.php
@@ -18,8 +18,8 @@ class Constructor {
        $result = true; //если без конструктора, то экземплярвсё же можно создать
        
        if (method_exists($className, '__construct')) {
-        $constructor = new \ReflectionMethod($className, '__construct');
-        $result = $constructor->isPublic();
+            $constructor = new \ReflectionMethod($className, '__construct');
+            $result = $constructor->isPublic();
        }
        
        return $result;
