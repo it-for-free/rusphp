e3258911dc5479e46835fffd9a457e5b8dd88ab6
vedro-compota <ru-ibn-hottab@yandex.ru>
2018-07-09 18:40:50 +0300

    Добавлена возможность получать поддомены

--- src/Network/Domain/Domain.php
+++ src/Network/Domain/Domain.php
@@ -3,19 +3,60 @@
 namespace ItForFree\rusphp\Network\Domain;
 
 use ItForFree\rusphp\Network\Url\Url;
+use ItForFree\rusphp\PHP\ArrayLib\Reverce;
 
 /**
- * Для работы с доменом
+ * Для работы со строкой домена сайта ("с доменом сайта").
+ * 
+ * Берет сам домен или из переданного в конструктор URL 
+ * или из $_SERVER['HTTP_HOST'] (учтите, что этот может быть подделан клиентом)
  */
 class Domain {
     
     /**
+     * Полное имя домена (без протокола), например
+     * subsub.sub2.example.com
+     * 
+     * @var string 
+     */
+    public $name;
+    
+    /** 
+     * Берет сам домен или из переданного в конструктор URL 
+     * или из $_SERVER['HTTP_HOST'] (учтите, что этот может быть подделан клиентом
+     * 
      * @param string $url строка, содержащая url (ссылку)
      */
-    public function __construct($url) 
+    public function __construct($url = '') 
     {
-        $Url = new Url($url);
+        if ($url) {
+            $Url = new Url($url);
+            $this->name = $Url->host;
+        } else {
+            $this->name = filter_input(INPUT_SERVER, 'HTTP_HOST');
+        }
 
+    }
+    
+    /**
+     * Получит поддомен нужного уровня, минимальный уровень -- 1
+     * Например для  subsub.sub2.example.com поддомены/уровни это:
+     * 4: subsub
+     * 3: sub2
+     * 2: example
+     * 1: com
+     * 
+     * @param int $level минимальный уровень =1
+     * @return string
+     * @throws Exception
+     */
+    public function getSubdomain($level = 1)
+    {
+        $subdomains = explode('.', $this->name);
+        
+        $subdomain = Reverce::getElement($subdomains, $level);
+        
+        return $subdomain;  
     }
     
     
--- src/PHP/ArrayLib/Reverce.php
+++ src/PHP/ArrayLib/Reverce.php
@@ -23,4 +23,23 @@ class Reverce
     {     
        return (($inArrayElementsCount - 1)- $afterReverceIndex + $startArrayIndexBeforeReverse);
     }
+    
+    /**
+     * Вернёт элемент массива по номеру, так как если бы порядок следования элементов был изменён на обратный
+     * null в случае отсутсвия элемента.
+     * 
+     * @param array $array      сам массив
+     * @param int $indexNumber  гипотетический номер элемента в отражённом массиве
+     * @retrun mixed  Элемент массива или null, если элемент отсутствует.
+     */
+    public static function getElement($array, $indexNumber) 
+    {
+        $result = null;
+        
+        if (isset($array[count($array) - $indexNumber])) {
+            $result = $array[count($array) - $indexNumber];
+        }
+        
+        return $result;
+    }
 }
