0c3da1dfa99116acbb904520727a4071fea904aa
vedro-compota <ru-ibn-hottab@yandex.ru>
2018-11-02 19:45:19 +0300

    Добавляем возможность проверки запуска php в командной строке.

---
+++ src/PHP/Sapi/Sapi.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Для работы с SAPI
+ * 
+ * @author vedro-compota
+ */
+class Sapi {
+    
+    /**
+     * Проверит запущен ли скрипт из командной строки
+     * [[check if run from command line]]
+     * 
+     * @return boolean
+     */
+    public static function isConsole()
+    {
+        return (php_sapi_name() == "cli");
+    }
+}
