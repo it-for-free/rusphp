d99fe6978cfc5e8c2a7ded6cf1a16d42cb8fb392
vedro-compota <ru-ibn-hottab@yandex.ru>
2018-11-29 20:09:53 +0300

    class

---
+++ src/Common/Latex/WikiMarkupToLatex.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of WikiMarkupToLatex
+ *
+ * @author qwe
+ */
+class WikiMarkupToLatex {
+    //put your code here
+}
