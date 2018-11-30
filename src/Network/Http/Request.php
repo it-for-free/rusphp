a6bf2ea951e6d634651781764a49987d4d79c19a
vedro-compota <ru-ibn-hottab@yandex.ru>
2018-11-30 17:04:38 +0300

    Класс для работы с параметрами запроса.

---
+++ src/Network/Http/Request.php
@@ -0,0 +1,31 @@
+<?php
+namespace ItForFree\rusphp\Network\Http;
+use ItForFree\rusphp\Network\Url\Url;
+
+/**
+ * Для работы с http-запросом
+ *
+ */
+class Request
+{
+    /**
+     * Возвращает ссылку на домен источник запроса, если его удалось определить,
+     *  в противном случае ip адрес
+     * 
+     * @return \ItForFree\rusphp\Network\Url\Url
+     */
+    public static function getOrigin()
+    {
+        if (array_key_exists('HTTP_ORIGIN', $_SERVER)) {
+            $origin = $_SERVER['HTTP_ORIGIN'];
+        }
+        else if (array_key_exists('HTTP_REFERER', $_SERVER)) {
+            $origin = $_SERVER['HTTP_REFERER'];
+        } else {
+            $origin = $_SERVER['REMOTE_ADDR'];
+        }
+        $originUrl = new Url($origin);
+        return $originUrl;
+    }        
+
+}
