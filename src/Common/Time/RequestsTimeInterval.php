a1e048372ae44a30246b5d487ff52595e16be411
vedro-compota <ru-ibn-hottab@yandex.ru>
2018-12-20 10:40:03 +0300

    Правка конфига и добавление класса для динамического рассчета интервала времени.

---
+++ src/Common/Time/RequestsTimeInterval.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Класс для определения эффективного временного интервала между запросами
+ * к некоторому API (напр. бесплатному).
+ * Цель: с одной стороны проверить быстрее, с другой не попасть в бан.
+ *  
+ * Как это работает:
+ * если при слишком частых запросах сервер перестаёт отвечать 
+ * корреткно, то данный класс автоматически будет увеличивать интервал
+ * в два раза, и уменьшать в два раза  при переходе 
+ * от полосы некорректных ответом в корректным.
+ * 
+ * Возможно, что есть и более успешные стратегии, 
+ * для которых класс можно унаследовать и переопределить методыю
+ *
+ *  (инервал времени, ожидание, пауза)
+ */
+class RequestsTimeInterval 
+{
+   /**
+    * Начальный временной интервал
+    * между запросами
+    * 
+    * @var int
+    */
+   public $startWaitInterval = 1;
+   
+   /**
+    * @param int $startWaitIterval стартовый интервал в секундах
+    */
+   public function __construct($startWaitInterval = 1) {
+       $this->startWaitInterval = $startWaitInterval;
+   }
+   
+   /**
+    * Вызывайте, чтобы сделать паузу между запросами    
+    */
+   public function wait()
+   {
+       sleep($this->baseIterval);
+   }
+   
+   /**
+    * Вызывайте после очередного запроса, чтобы динамически обновить интервал
+    * 
+    * @param boolean $isLastResponceCorrect корректен ли 
+    *    (с точки зрения внутренней логики вашего приложения)
+    *    ответ последнего запроса, т.е. если его НЕ надо дублировать,
+    *    то передавайте true.
+    */
+   public function update($isLastResponceCorrect)
+   {
+       $this->setNewIntervalValue($isLastResponceCorrect);
+   }
+   
+   /**
+    * 
+    * @staticvar boolean $isPreviousResponceOk  Хранит статус завершения предыдущего 
+    * (относительно того, для которого вы сейчас можете вызвать этот метод) зароса.
+    * @param  boolean $isNewResponceOk  корректен ли 
+    *    (с точки зрения внутренней логики вашего приложения)
+    *    ответ последнего запроса, т.е. если его НЕ надо дублировать,
+    *    то передавайте true.
+    * @return null
+    */
+    protected function setNewIntervalValue($isNewResponceOk)
+    {
+       static $isPreviousResponceOk = true; // до начала работы будем считать что все ок)
+       
+       if ($isPreviousResponceOk && $isNewResponceOk) {
+           return; // просто выходим, ничего не меняя, если продолжается удачная полоса
+       } else if (!$isPreviousResponceOk && $isNewResponceOk)  {
+           $this->startWaitInterval = intdiv($this->startWaitInterval, 2); // уменьшаем в 2 раза при переходе к удачной полосе
+           $isPreviousResponceOk = $isNewResponceOk;
+           return;
+       } else if (!$isPreviousResponceOk && !$isNewResponceOk)  {
+           $this->startWaitInterval = $this->startWaitInterval * 2; // если неудачная полоса продолжается, то увеличиваем в 2 раза.
+           return;
+       }
+       
+   }
+}
