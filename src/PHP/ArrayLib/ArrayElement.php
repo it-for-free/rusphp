ea334ac9c064abe826ee45307c94ab9bf92fede9
vedro-compota <ru-ibn-hottab@yandex.ru>
2019-02-05 15:43:16 +0300

    Добавлена функция получения случайного элемента массива + оформление.

--- src/PHP/ArrayLib/ArrayElement.php
+++ src/PHP/ArrayLib/ArrayElement.php
@@ -17,4 +17,18 @@ class ArrayElement
     {
         return array_search($key, array_keys($arr));
     }
+    
+    /**
+     * Вернет случайный элемент массива, 
+     * работает на основе стандартной array_rand()
+     * 
+     * ((php get random element from array))
+     * 
+     * @param mixed[] $arr  массив значений произвольного типа
+     * @return mixed    случайно выбранный элмент массива
+     */
+    public static function getRandom($arr)
+    {
+        return $arr[array_rand($arr, 1)];
+    }
 }
