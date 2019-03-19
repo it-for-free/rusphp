912eb329dc61b2ae4c984a2a34cb665c5a165fe8
vedro-compota <ru-ibn-hottab@yandex.ru>
2019-03-19 22:48:22 +0300

    Добработка

--- src/Common/Ui/Breadcrumbs/Breadcrumbs.php
+++ src/Common/Ui/Breadcrumbs/Breadcrumbs.php
@@ -10,12 +10,34 @@ class Breadcrumbs
     /**
      * @var BreadcrumbsElement[] массив элементов-крошек
      */
-    public $parents;
+    protected $parents;
     
     
     /**
      * @var srting название текущего (последнего в списке, самого правого)
      *  элемента (обычно оно не делается ссылкой)
      */
-    public $current; 
+    public $current;
+    
+    /**
+     *  Добавляем очередной элемент
+     * 
+     * @param string $text Выводимый текст (имя)
+     * @param string $url  ссылка (url)
+     */
+    public function add($text, $url)
+    {
+       $this->parents[] = new BreadcrumbsElement($text, $url); 
+    }
+    
+    /**
+     * Вернёт массив родительских элементов 
+     * (название текущего элемента получайте отдельно)
+     * 
+     * @return BreadcrumbsElement[]  массив родительских элементов
+     */
+    public function get()
+    {
+       $parents[] = new BreadcrumbsElement($text, $url); 
+    }
 }
--- src/Common/Ui/Breadcrumbs/BreadcrumbsElement.php
+++ src/Common/Ui/Breadcrumbs/BreadcrumbsElement.php
@@ -17,4 +17,15 @@ class BreadcrumbsElement
      * @var srting ссылка (url)
      */
     public $url; 
+    
+    /**
+     * Элемент "хлебных крошек"
+     * 
+     * @param string $text Выводимый текст (имя)
+     * @param string $url  ссылка (url)
+     */
+    public function __construct($text, $url = '') {
+        $this->text = $text;
+        $this->url = $url;
+    }
 }
