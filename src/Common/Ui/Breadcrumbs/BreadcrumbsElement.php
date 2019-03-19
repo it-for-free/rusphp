7aab2b22ce8db2896f9ce27ce04e00659a819a3e
vedro-compota <ru-ibn-hottab@yandex.ru>
2019-03-19 18:37:12 +0300

    Добавлен класс для хлебных крошек.

---
+++ src/Common/Ui/Breadcrumbs/BreadcrumbsElement.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ItForFree\rusphp\Common\Ui\Breadcrumbs;
+
+/**
+ * Элемент "хлебных крошек"
+ */
+class BreadcrumbsElement 
+{ 
+    /**
+     * @var srting Выводимый текст (имя) 
+     */
+    public $text;
+    
+    
+    /**
+     * @var srting ссылка (url)
+     */
+    public $url; 
+}
