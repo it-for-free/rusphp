d65cc0439b62d117f75f2d46da460a3164fbea37
vedro-compota <ru-ibn-hottab@yandex.ru>
2019-03-29 19:51:58 +0300

    Добавление метода сравнения даты с текущей в днях.

--- src/PHP/DateTime/DateTimeDifference.php
+++ src/PHP/DateTime/DateTimeDifference.php
@@ -16,22 +16,39 @@ class DateTimeDifference
  
     /**
      * Вычислит число лет прошедших к сегодняшнему моменту начиная с даты,
-     * хранящейся в $fromDateTime
+     * хранящейся в $startDateTime
      * (можно использовать для вычисления возраста по дате рождения)
      * 
-     * @param \DateTime $$startDateTime  точка времени (подразумевается, что в прошлом)
+     * @param \DateTime $startDateTime  точка времени (подразумевается, что в прошлом)
      * @return int число прошедших лет
      */
     public static function yearsByCurrentMoment($startDateTime)
     {
-        $tz  = new DateTimeZone('Europe/Brussels'); // зона не имеет значения в данном случае
+        $tz  = new \DateTimeZone('Europe/Brussels'); // зона не имеет значения в данном случае
         $years = $startDateTime
-                ->diff(new DateTime('now', $tz)) 
+                ->diff(new \DateTime('now', $tz)) 
                 ->y;
      
         return $years;
     }
     
+    /**
+     * Вычислит число дней прошедших к сегодняшнему моменту начиная с даты,
+     * хранящейся в $startDateTime
+     * (можно использовать для вычисления возраста по дате рождения)
+     * 
+     * @param \DateTime $startDateTime  точка времени (подразумевается, что в прошлом)
+     * @return int число прошедших дней
+     */
+    public static function daysByCurrentMoment($startDateTime)
+    {
+        $tz  = new \DateTimeZone('Europe/Brussels'); // зона не имеет значения в данном случае
+        $days = $startDateTime
+                ->diff(new \DateTime('now', $tz)) 
+                ->days;
+     
+        return $days;
+    }
     
     /**
      * Разница в днях между двумя датами
@@ -44,6 +61,5 @@ class DateTimeDifference
     {
         $days  = $startDateTime->diff($endDateTime)->days;
         return $days;
-    }
-                   
+    }               
 }
