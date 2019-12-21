b95f06a6980110a1d330c39f06e7f8de5287e55b
vedro-compota <ru-ibn-hottab@yandex.ru>
2019-12-21 16:09:04 +0300

    Добавлено: Список mime по списку расширений файлов.

--- src/File/MIME.php
+++ src/File/MIME.php
@@ -7,7 +7,12 @@ namespace ItForFree\rusphp\File;
  */
 class MIME
 {
-    
+    /**
+     * mime тип для данного расширения
+     * 
+     * @param string $fileExtension
+     * @return string
+     */
     public static function byExtention($fileExtension)
     {
         $mimes = new \Mimey\MimeTypes;
@@ -15,5 +20,29 @@ class MIME
         return $mimes->getMimeType($fileExtension); 
     }
     
-    
+    /**
+     * Все mime типы для данных расшерений (или расшерения)
+     * 
+     * @param string|array $fileExtensions  расширения (или одно), для которых нужно получить список mime-типов
+     * @return array of string
+     */
+    public static function getAllbyExtentions($fileExtensions)
+    {
+        $mimes = new \Mimey\MimeTypes;
+        
+        $results = [];
+        
+        if (is_array($fileExtensions)) {
+            foreach ($fileExtensions as $ext) {
+                $extMimes = $mimes->getAllMimeTypes($ext);
+                foreach ($extMimes  as $mimeType) {
+                    $results[] = $mimeType;
+                }
+            }
+        } else {
+            $results = $mimes->getAllMimeTypes($fileExtensions);
+        }
+
+        return array_unique($results); 
+    }
 }
