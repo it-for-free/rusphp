d61f9e1e3d9ebdd05a7c8afc0495d7c72778064c
nazarevrn <kimi-champ@mail.ru>
2020-12-27 14:14:19 +0300

    improve universal constructor stage 2

--- src/PHP/Object/ObjectFactory.php
+++ src/PHP/Object/ObjectFactory.php
@@ -33,8 +33,13 @@ class ObjectFactory {
    }
 
     /**
-     * @param string $classname
-     * @param array $config
+     * Создает объект через конструктор.
+     * @param string $classname имя класса для создания объекта
+     * @param array $config массив аргументов для конструктора
+     * Порядок следования аргументов не обязательно должен совпадать с порядком, который ожидает конструктор.
+     * Если массив ассоциативный, то он должен быть вида [propertyName => propertyValue]
+     * Если массив вида [0, 10, 'string'],
+     * а конструктор ожидает sting, int, int ему будут переданы string, 0, 10 входного массива
      * @return null|object
      * @throws \ReflectionException
      */
@@ -47,24 +52,64 @@ class ObjectFactory {
            $class = new \ReflectionClass($classname);
            $classConstruct = $class->getConstructor();
            $constructParams = $classConstruct->getParameters();
+           $constructorData = [];
            foreach ($constructParams as $param) {
                $constructParamType = $param->getType()->getName();
-               foreach ($config as $property => $value) {
+               $constructorPropertyName = $param->getName();
+               $constructorData[$constructorPropertyName] = $constructParamType;
+           }
+           //ключи ассоциативного массива, переданного в метод, содержат названия всех аргументов,
+           // которые ожидает конструктор
+           if (self::isArrayKeysEqual($constructorData, $config) === true) {
+               foreach ($constructorData as $propertyName => $propertyType) {
+                   $value = $config[$propertyName];
                    $type = self::getType($value);
-                   if ($constructParamType === $type) {
-                       $sorted[$property] = $value;
-                       unset($config[$property]);
+                   if ($type === $propertyType) {
+                       $sorted[$propertyName] = $value;
+                       unset($config[$propertyName]);
+                   }
+               }
+           } else {
+               //$config - не ассоциативный. разрулим все на основе сравнения типов
+               foreach ($constructorData as $propertyName => $propertyType) {
+                   foreach ($config as $property => $value) {
+                       $type = self::getType($value);
+                       if ($propertyType === $type) {
+                           $sorted[$property] = $value;
+                           unset($config[$property]);
+                       }
                    }
                }
            }
-
            $resultObject = $class->newInstanceArgs($sorted);
-       }
+
+           }
 
        return $resultObject;
    }
 
     /**
+     * Сравнивает ключи двух массивов.
+     * Вернёт true, если ключи одного массива равны ключам другого
+     * не зависимо от порядка их следования
+     * @param $first array первый массив
+     * @param $second array второй массив
+     * @return bool
+     */
+   private static function isArrayKeysEqual(array $first, array $second): bool
+   {
+
+       $keysFirst = array_keys($first);
+       $keysSecond = array_keys($second);
+       asort($keysFirst);
+       asort($keysSecond);
+       return ($keysFirst === $keysSecond);
+   }
+
+    /**
+     * Функция gettype для типа integer вернёт тип integer.
+     * Но в свойствах конструктора тип integer называется int.
+     *
      * @param null $value
      * @return false|string
      */
@@ -82,6 +127,7 @@ class ObjectFactory {
             case 'boolean' :
                 $type = 'bool';
                 break;
+            //gettype для float по историческим причинам возвращает double
             case 'double' :
                 $type = 'float';
                 break;
